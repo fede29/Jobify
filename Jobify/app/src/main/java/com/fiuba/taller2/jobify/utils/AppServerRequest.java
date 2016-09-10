127766b7acdcd7cd03bdbea25596fad9c571f3d6
JuanmaLambre <juanmlambre@gmail.com>
2016-09-10 17:04:35 -0300

    Close #4

--- Jobify/app/src/main/java/com/fiuba/taller2/jobify/utils/AppServerRequest.java
+++ Jobify/app/src/main/java/com/fiuba/taller2/jobify/utils/AppServerRequest.java
@@ -3,9 +3,12 @@ package com.fiuba.taller2.jobify.utils;
 
 import com.fiuba.taller2.jobify.User;
 
+import org.json.JSONObject;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -16,21 +19,23 @@ public class AppServerRequest {
     private static final String BASE_URL = "http://192.168.0.109:5000/";
     private static final OkHttpClient client = new OkHttpClient();
     private static String token;
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     public static void updateToken(String t) {
         token = t;
     }
 
     public static void login(String email, String password, Callback callback) {
-        // TODO: Modify parameters, send via JSON
-        RequestBody formBody = new FormBody.Builder().build();
-        Request request = new Request.Builder().url(BASE_URL + RequestConstants.Routes.LOGIN)
-                .post(formBody)
-                .addHeader(RequestConstants.UserParams.EMAIL, email)
-                .addHeader(RequestConstants.UserParams.PASSWORD, password)
-                .build();
-        Call call = client.newCall(request);
-        call.enqueue(callback);
+        JSONObject params = new JSONObject();
+        try {
+            params.put(RequestConstants.UserParams.EMAIL, email);
+            params.put(RequestConstants.UserParams.PASSWORD, password);
+        } catch (Exception e) {
+            // TODO: Log
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(JSON, params.toString());
+        post(BASE_URL + RequestConstants.Routes.LOGIN, callback, body);
     }
 
     public static void getContacts(User user, Callback contactsLoadCallback) {
@@ -44,7 +49,19 @@ public class AppServerRequest {
     }
 
     public static void get(String url, Callback callback) {
-        Request request = new Request.Builder().url(url).get().build();
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+
+    public static void post(String url, Callback callback, RequestBody body) {
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
         Call call = client.newCall(request);
         call.enqueue(callback);
     }
