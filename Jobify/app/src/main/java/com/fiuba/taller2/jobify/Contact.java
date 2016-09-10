7e062545ca3a73f3a76c4c53c71921397d3080ef
JuanmaLambre <juanmlambre@gmail.com>
2016-09-10 17:05:01 -0300

    Add default user picture file

---
+++ Jobify/app/src/main/java/com/fiuba/taller2/jobify/Contact.java
@@ -0,0 +1,44 @@
+package com.fiuba.taller2.jobify;
+
+import com.fiuba.taller2.jobify.constant.JSONConstants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+
+public class Contact implements Serializable {
+
+    private String firstName, lastName, profilePicUrl;
+
+    public static Contact hydrate(JSONObject thisJson) {
+        Contact c = new Contact();
+        try {
+            c.firstName = thisJson.getString(JSONConstants.Contact.FIRST_NAME);
+            c.lastName = thisJson.getString(JSONConstants.Contact.LAST_NAME);
+            c.setPictureURL(
+                    thisJson.isNull(JSONConstants.Contact.PROFILE_PIC) ?
+                            null : thisJson.getString(JSONConstants.Contact.PROFILE_PIC)
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public String getFullName() {
+        return firstName + ' ' + lastName;
+    }
+
+    public String getPictureURL() {
+        return profilePicUrl;
+    }
+
+    public void setPictureURL(String url) {
+        profilePicUrl = url;
+    }
+
+    public Boolean hasProfilePic() {
+        return profilePicUrl != null;
+    }
+}
