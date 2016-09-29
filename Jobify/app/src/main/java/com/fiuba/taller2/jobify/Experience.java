2d604353a2c55717056c90269c6f53f37944a68f
JuanmaLambre <juanmlambre@gmail.com>
2016-09-29 16:56:35 -0300

    Add Experience files

---
+++ Jobify/app/src/main/java/com/fiuba/taller2/jobify/Experience.java
@@ -0,0 +1,21 @@
+package com.fiuba.taller2.jobify;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.Serializable;
+
+public class Experience implements Serializable {
+
+    @Expose @SerializedName("id")           int id;
+    @Expose @SerializedName("where")        String place;
+    @Expose @SerializedName("job_position") JobPosition jobPosition;
+
+    public String getPlace() {
+        return place;
+    }
+
+    public JobPosition getJobPosition() {
+        return jobPosition;
+    }
+}
